b9eb7c038a2e543b026a38d81aa5902ae0903d8e
Ibrar Ahmad <90978293+IbrarAhmad969@users.noreply.github.com>
2025-07-22 12:54:20 +0500

    feat: update package management and add cross-env for environment variable handling - Update both the pkg.json files so that code be run on single terminal
    - Generate a build folder and did the setup, added navigation.

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require("path")
 const planetsRouter = require('./routes/planets/planets.router');
 
 const cors = require('cors');
@@ -10,6 +11,12 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+ 
+
 app.use(planetsRouter);
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+})
 
 module.exports = app;
