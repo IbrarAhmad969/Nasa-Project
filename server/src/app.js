54e488561ac3f99ab96d5d89721beb7c14fb7ec6
Ibrar Ahmad <90978293+IbrarAhmad969@users.noreply.github.com>
2025-07-24 10:18:40 +0500

    implement launch creation functionality and update routes

--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const path = require("path")
 const morgan = require("morgan");
 
-const { getAllLaunches } = require('./routes/launches/launches.controller');
-
 const planetsRouter = require('./routes/planets/planets.router');
 const launchesRouter = require('./routes/launches/launches.router');
 
@@ -24,6 +22,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use("/planets", planetsRouter);
 app.use("/launches", launchesRouter);
+
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, "..", "public", "index.html"))
--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,11 +1,13 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 
 const launch = {
     flightNumber: 100,
     missionName: 'Kepler Exploration X',
     rocket: 'Explorer ISI',
-    launchData: new Date('Dec 27, 2030'),
+    launchDate: new Date('Dec 27, 2030'),
     destination: 'Kepler-442 b',
     customer: ['ZTM', 'NASA'],
     upcoming: true,
@@ -19,6 +21,20 @@ function getAllLaunches(){
     return Array.from(launches.values());
 }
 
+function addNewLaunch(launch){
+
+    latestFlightNumber++;
+    launches.set(latestFlightNumber, Object.assign(launch, {
+
+        upcoming: true,
+        success: true,
+        customers: ['Zero to Mastery', 'NASA'],
+        flightNumber: latestFlightNumber,
+    }));
+
+}
+
 module.exports = {
     getAllLaunches,
+    addNewLaunch,
 }
