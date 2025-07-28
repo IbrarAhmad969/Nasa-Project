1784f8b503a7ab4f239c41662d2069c95b751fc9
Ibrar Ahmad <90978293+IbrarAhmad969@users.noreply.github.com>
2025-07-28 15:06:43 +0500

    refactor: improve formatting and consistency in launches test cases

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,23 +1,46 @@
 const request = require("supertest");
 const app = require("../../app");
 
-
-describe('Test GET / launches', ()=>{
-    test('It should respond with 200 success', async()=>{
+describe('Test GET / launches', () => {
+    test('It should respond with 200 success', async () => {
         const response = await request(app).get('/launches')
-        .expect('Content-Type', /json/)
-   
+            .expect('Content-Type', /json/)
+
     })
 })
 
-describe('Test POST / launches', ()=>{
-    test('It should respond with 200 success, ', ()=>{
+describe('Test POST / launches', () => {
+    const completeLaunchData = {
+        mission: "USS Enterprise",
+        rocket: "NCC 1701-F",
+        target: "Kepler-186 f",
+        launchDate: "January 4, 2028"
+    }
+    const launchDataWithoutDate = {
+        mission: "USS Enterprise",
+        rocket: "NCC 1701-F",
+        target: "Kepler-186 f",
+    }
+    test('It should respond with 201 created, ', async () => {
+        const response = await request(app)
+            .post('/launches')
+            .send(completeLaunchData)
+            .expect('Content-Type', /json/)
+            .expect(201);
+
+            const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+            const responseDate = new Date(response.body.launchDate).valueOf();
+            expect(responseDate).toBe(requestDate);
+
+        expect(response.body).toMatchObject(
+            launchDataWithoutDate
+        )
 
     });
-    test('It should catch missing required properties ', ()=>{
+    test('It should catch missing required properties ', () => {
 
     });
-    test('It should catch invalid dates', ()=>{
-         
+    test('It should catch invalid dates', () => {
+
     })
 })
