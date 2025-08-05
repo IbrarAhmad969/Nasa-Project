0d87fbdc3b7aa584b2757ecf89b723cdf705434c
Ibrar Ahmad <90978293+IbrarAhmad969@users.noreply.github.com>
2025-08-05 07:57:00 +0500

    Enhance server configuration and improve planets model
    - Updated package.json to include PM2 for process management and added a cluster script for scaling.
    - Refactored planets.model.js for improved readability and consistency in code style.
    - Ensured proper formatting and indentation in the isHabitablePlanet and loadPlanetsData functions.

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,53 +1,53 @@
-const fs = require('fs');
+const fs = require("fs");
 const path = require("path");
 const { parse } = require("csv-parse");
-
 
 const habitablePlanets = [];
 
 function isHabitablePlanet(planet) {
-    return planet['koi_disposition'] === 'CONFIRMED'
-        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
-        && planet['koi_prad'] < 1.6;
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
 }
 
 function loadPlanetsData() {
-    return new Promise((resolve, reject) => {
-
-        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
-            .pipe(parse({
-                comment: "#",
-                columns: true,
-            }))
-            .on('data', (data) => {
-                if (isHabitablePlanet(data)) {
-                    habitablePlanets.push({
-                        keplerName: data['kepler_name'], // 🌟 normalize here
-                        
-                        ...data,
-                    });
-                }
-            })
-            .on('error', (err) => {
-                console.log(err);
-                reject(err);
-            })
-            .on('end', () => {
-
-                console.log(`${habitablePlanets.length} Habitable planets found! `);
-                resolve();
-
-            })
-
-    })
-
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
+    )
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push({
+            keplerName: data["kepler_name"],
+            ...data,
+          });
+        }
+      })
+      .on("error", (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on("end", () => {
+        console.log(`${habitablePlanets.length} Habitable planets found! `);
+        resolve();
+      });
+  });
 }
 
 function getAllPlanets() {
-    return habitablePlanets;
+  return habitablePlanets;
 }
 
 module.exports = {
-    loadPlanetsData,
-    getAllPlanets,
+  loadPlanetsData,
+  getAllPlanets,
 };
