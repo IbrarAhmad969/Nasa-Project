61c3f4a141b72594e8490c5c91dbdceb6eb48e89
Ibrar Ahmad <90978293+IbrarAhmad969@users.noreply.github.com>
2025-08-25 16:39:14 +0500

    Jest Test work for classic array Implementation, but for Real DB jest needs some extra setup, fixed the "jest" in package.json file to work with MongoDB.
    - Jest needs to connect DB before the request, previous code ConnectDB was coupled, and DRY principle couldn't be used, moved the ConnectDB call to services, and implemented the required JEST test, and connection with Service with One piece of Code.
    
    - write another block to connect DB, and run all the tests in test file.

--- server/src/index.js
+++ server/src/index.js
@@ -1,26 +1,15 @@
-const PORT = process.env.PORT || 8000
-const app = require('./app');
-const http = require('http');
-const { loadPlanetsData } = require("./models/planets.model")
-const mongoose = require("mongoose")
+const { loadPlanetsData } = require("./models/planets.model");
+const { mongoConnect } = require("./services/mongo");
 
-const server = http.createServer(app)
 
 async function loadServer() {
     try {
-        await mongoose.connect('mongodb+srv://ibrarhashmi206:w2EZXI6n7TAK17W1@cluster0.nmb1i9g.mongodb.net/NasaProject');
-        console.log("Connected Successfully");
-        await loadPlanetsData()
-        server.listen(PORT, () => {
-            console.log(`Listening on PORT ${PORT}`);
-        });
-
-
+        await mongoConnect();
+        await loadPlanetsData();
     } catch (error) {
-        console.error('Error While connecting the DB ', error.message)
+        console.error("Failed to start server:", error);
         process.exit(1);
     }
-
 }
 
 loadServer();
---
+++ server/src/services/mongo.js
@@ -0,0 +1,24 @@
+const PORT = process.env.PORT || 8000
+
+const mongoose = require("mongoose");
+const app = require('../app');
+const http = require('http');
+
+const server = http.createServer(app);
+
+async function mongoConnect() {
+    try {
+        await mongoose.connect('mongodb+srv://ibrarhashmi206:w2EZXI6n7TAK17W1@cluster0.nmb1i9g.mongodb.net/NasaProject');
+        console.log("Connected Successfully");
+        server.listen(PORT, () => {
+            console.log(`Listening on PORT ${PORT}`);
+        });
+
+    } catch (error) {
+        console.error('Error While connecting the DB ', error.message)
+        process.exit(1);
+    }
+}
+module.exports = {
+    mongoConnect
+}
